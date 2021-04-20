acbc69391d7d9a2c83899a078ac365b2ab0d8ab2
USER <USER@DESKTOP-3K2FJ30>
2021-04-20 07:43:14 +0530

    updated-second commit

--- First_Spring_Boot_Application_Demo/src/main/java/com/infotech/com/FirstSpringBootApplicationDemoApplication.java
+++ First_Spring_Boot_Application_Demo/src/main/java/com/infotech/com/FirstSpringBootApplicationDemoApplication.java
@@ -14,5 +14,8 @@ public class FirstSpringBootApplicationDemoApplication {
 	public static void  main1() {
 		System.out.println("hello");
 	}
+	public static void  main2() {
+		System.out.println("hello2");
+	}
 	
 }
