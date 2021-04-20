ed4cf874e9b21e4d4de5381847094c78109fc5bb
USER <USER@DESKTOP-3K2FJ30>
2021-04-20 07:39:42 +0530

    First-Commit

---
+++ First_Spring_Boot_Application_Demo/src/main/java/com/infotech/com/FirstSpringBootApplicationDemoApplication.java
@@ -0,0 +1,18 @@
+package com.infotech.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplicationDemoApplication.class, args);
+	}
+
+	
+	public static void  main1() {
+		System.out.println("hello");
+	}
+	
+}
